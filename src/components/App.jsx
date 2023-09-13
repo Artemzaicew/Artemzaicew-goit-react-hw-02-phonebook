a733931231f29c2761faa5ec3cd9098c41c25bb4
Artemzaicew <122361190+Artemzaicew@users.noreply.github.com>
2023-09-13 17:53:09 +0300

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import ContactsForm from "ContactsForm/ContactsForm";
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+   
+  };
+
+  createContact =(data)=>{
+    console.log(data)
+  }
+
+  render() {
+    return (
+      // <div
+      //   style={{
+      //     height: '100vh',
+      //     display: 'flex',
+      //     justifyContent: 'center',
+      //     alignItems: 'center',
+      //     fontSize: 40,
+      //     color: '#010101'
+      //   }}
+      // ></div>
+      <ContactsForm createContact={this.createContact}/>
+    );
+  }
 };
